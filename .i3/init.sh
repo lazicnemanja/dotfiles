3bbc74d92c2377fd29da7495cd11b8b04e6e9512
Nemanja Lazic <nemanja.lazic@gmail.com>
2017-03-07 10:55:37 -0500

    init

---
+++ .i3/init.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+## ubuntu ##
+sudo apt-get install  i3lock i3status i3blocks feh fonts-font-awesome rofi
+
+# freebsd
+#sudo pkg install i3 i3lock i3status feh fonts-font-awesome uxterm
+
+# conf dir cp
+mkdir -p ~/.i3
+cp -R * ~/.i3/
+
